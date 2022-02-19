e68c9b12637c25510b641bfbaa64075c29d6dd57
Antonio-Peer <95565154+Antonio-Peer@users.noreply.github.com>
2022-02-19 08:01:27 -0800

    "Submission for HW3"

--- HW3/Bullseye with SwiftUI/Bullseye with SwiftUI/ContentView.swift
+++ HW3/Bullseye with SwiftUI/Bullseye with SwiftUI/ContentView.swift
@@ -7,15 +7,53 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State var number = 25
+    @State var sliderValue: Double = 50
+    @State var maxSliderValue: Double = 100
+    @State var toggle = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack() {
+            HStack() {
+                VStack() {
+                    Text("High Score:")
+                    Text("0")
+                }
+                .padding(.leading, 20)
+                
+                Spacer()
+                
+                VStack() {
+                    Text("Current Level:")
+                    Text("1")
+                }
+                .padding(.trailing, 20)
+            }
+            
+            Spacer()
+            
+            VStack(){
+                Text("Move the slider to:")
+                    .font(.system(size: 30))
+                Text(String(number))
+                    .font(.system(size: 30))
+                Slider(value: $sliderValue, in: 0...maxSliderValue)
+                    .padding(.leading, 15)
+                    .padding(.trailing, 15)
+                //Button("Check")
+            }
+            
+            Spacer()
+            
+            VStack(){
+                Text("Exact Mode")
+                Toggle("Toggle", isOn: $toggle).labelsHidden()
+            }
+        }
+            .padding(.top, 20)
+            .padding(.bottom, 20)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
