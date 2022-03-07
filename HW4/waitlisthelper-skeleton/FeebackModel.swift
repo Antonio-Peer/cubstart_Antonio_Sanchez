b02735bc483742b34763322340d048a5bd823063
Antonio-Peer <95565154+Antonio-Peer@users.noreply.github.com>
2022-03-06 21:28:36 -0800

    HW4 Final Submission

---
+++ HW4/waitlisthelper-skeleton/FeebackModel.swift
@@ -0,0 +1,18 @@
+//
+//  FeebackModel.swift
+//  waitlisthelper-skeleton
+//
+//  Created by Antonio Sanchez on 3/6/22.
+//
+
+import Foundation
+
+let feedbackModel =
+    [(90, "You're good"),
+     (60, "You're probably ok"),
+     (40, "Idk, it's like 50-50"),
+     (20, "You're probably not getting off"),
+     (0, "Rip")
+    ]
+
+var test = "test"
